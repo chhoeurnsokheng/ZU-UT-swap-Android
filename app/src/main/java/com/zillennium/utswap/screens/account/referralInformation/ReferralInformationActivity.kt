501b2c6041f2d1c0169fbac26279eb45183b018d
bpanhchakrith <b.panhchakrith@gmail.com>
2022-06-21 11:08:05 +0700

    update account layout

--- app/src/main/java/com/zillennium/utswap/screens/account/referralInformation/ReferralInformationActivity.kt
+++ app/src/main/java/com/zillennium/utswap/screens/account/referralInformation/ReferralInformationActivity.kt
@@ -1,5 +1,10 @@
 package com.zillennium.utswap.screens.account.referralInformation
 
+import android.content.ClipData
+import android.content.Context
+import android.os.Build
+import android.text.ClipboardManager
+import android.widget.Toast
 import com.zillennium.utswap.R
 import com.zillennium.utswap.bases.mvp.BaseMvpActivity
 import com.zillennium.utswap.databinding.ActivityReferralInformationBinding
@@ -19,6 +24,36 @@ class ReferralInformationActivity :
 
                 imgClose.setOnClickListener {
                     finish()
+                }
+
+                layReferralId.setOnClickListener {
+                    val sdk = Build.VERSION.SDK_INT
+                    if (sdk < Build.VERSION_CODES.HONEYCOMB) {
+                        val clipboard =
+                            getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        clipboard.text = "PQRSTU"
+                    } else {
+                        val clipboard =
+                            getSystemService(Context.CLIPBOARD_SERVICE) as android.content.ClipboardManager
+                        val clip = ClipData.newPlainText("text label", "PQRSTU")
+                        clipboard.setPrimaryClip(clip)
+                    }
+                    Toast.makeText(this@ReferralInformationActivity,"Reference ID was Copied",Toast.LENGTH_SHORT).show()
+                }
+
+                layReferralLink.setOnClickListener {
+                    val sdk = Build.VERSION.SDK_INT
+                    if (sdk < Build.VERSION_CODES.HONEYCOMB) {
+                        val clipboard =
+                            getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        clipboard.text = "https://utswap.io/referral/PQRSTU"
+                    } else {
+                        val clipboard =
+                            getSystemService(Context.CLIPBOARD_SERVICE) as android.content.ClipboardManager
+                        val clip = ClipData.newPlainText("text label", "https://utswap.io/referral/PQRSTU")
+                        clipboard.setPrimaryClip(clip)
+                    }
+                    Toast.makeText(this@ReferralInformationActivity,"Reference Link was Copied",Toast.LENGTH_SHORT).show()
                 }
 
             }
