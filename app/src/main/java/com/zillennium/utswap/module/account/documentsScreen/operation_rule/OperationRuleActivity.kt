5226cfbb47aadadc93154e4a96194c46629c3379
Sokheng chhoeurn <>
2022-07-18 11:19:53 -0700

    Fixed conflict

---
+++ app/src/main/java/com/zillennium/utswap/module/account/documentsScreen/operation_rule/OperationRuleActivity.kt
@@ -0,0 +1,12 @@
+package com.zillennium.utswap.module.account.documentsScreen.operation_rule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zillennium.utswap.R
+
+class OperationRuleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operation_rule)
+    }
+}
