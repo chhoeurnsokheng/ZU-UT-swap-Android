fe5e533cf2c9cdd723c038390c64d049f8b3cdbc
Sokheng chhoeurn <>
2022-07-18 11:54:20 -0700

    add one more condition of siginIn

--- app/src/main/java/com/zillennium/utswap/module/account/documentsScreen/contract/ContractActivity.kt
+++ app/src/main/java/com/zillennium/utswap/module/account/documentsScreen/contract/ContractActivity.kt
@@ -1,12 +1,35 @@
 package com.zillennium.utswap.module.account.documentsScreen.contract
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zillennium.utswap.R
+import com.zillennium.utswap.bases.mvp.BaseMvpActivity
+import com.zillennium.utswap.databinding.ActivityContractBinding
 
-class ContractActivity : AppCompatActivity() {
+class ContractActivity :  BaseMvpActivity<ContractView.View, ContractView.Presenter, ActivityContractBinding>(),
+    ContractView.View {
+    override val layoutResource: Int = R.layout.activity_contract
+    override var mPresenter: ContractView.Presenter = ContractPresenter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contract)
+
+    }
+
+    override fun initView() {
+        super.initView()
+        toolBar()
+    }
+
+
+    private fun toolBar(){
+        setSupportActionBar(binding.includeLayout.tb)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_left)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.includeLayout.apply {
+            tbTitle.setText(R.string.contract)
+            tb.setNavigationOnClickListener {
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/zillennium/utswap/module/account/documentsScreen/operation_rule/OperationRuleActivity.kt
+++ app/src/main/java/com/zillennium/utswap/module/account/documentsScreen/operation_rule/OperationRuleActivity.kt
@@ -1,12 +1,37 @@
 package com.zillennium.utswap.module.account.documentsScreen.operation_rule
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zillennium.utswap.R
+import com.zillennium.utswap.bases.mvp.BaseMvpActivity
+import com.zillennium.utswap.databinding.ActivityOperationRuleBinding
 
-class OperationRuleActivity : AppCompatActivity() {
+class OperationRuleActivity : BaseMvpActivity<OperationRuleView.View, OperationRuleView.Presenter, ActivityOperationRuleBinding>(),
+    OperationRuleView.View  {
+
+    override var mPresenter: OperationRuleView.Presenter = OperationRulePresenter()
+    override val layoutResource: Int = R.layout.activity_operation_rule
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_operation_rule)
+
+    }
+
+
+    override fun initView() {
+        super.initView()
+        toolBar()
+    }
+
+
+    private fun toolBar(){
+        setSupportActionBar(binding.includeLayout.tb)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_left)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.includeLayout.apply {
+            tbTitle.setText(R.string.privacy_policy)
+            tb.setNavigationOnClickListener {
+                finish()
+            }
+        }
     }
 }
