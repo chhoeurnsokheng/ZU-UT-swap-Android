d95ee1c1df4512d25d77036d34b989d7cdf370dd
Khem Rothana <khemrothana99@gmail.com>
2022-07-28 09:01:50 +0700

    view image from project detail

--- app/src/main/java/com/zillennium/utswap/module/project/ViewImage/ImageViewActivity.kt
+++ app/src/main/java/com/zillennium/utswap/module/project/ViewImage/ImageViewActivity.kt
@@ -54,6 +54,8 @@ class ImageViewActivity
             }
             ivClose.setOnClickListener {
                 finish()
+                this@ImageViewActivity.overridePendingTransition(0, 0)
+
             }
 
         }
