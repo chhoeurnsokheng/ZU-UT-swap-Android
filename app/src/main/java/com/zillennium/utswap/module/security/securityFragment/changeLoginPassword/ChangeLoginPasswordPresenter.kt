20994b9427d836e466b1ec8ad622214f5da82b9b
Eangly <teeangly77@gmail.com>
2022-08-30 11:15:54 +0700

    change login and fund password

--- app/src/main/java/com/zillennium/utswap/module/security/securityFragment/changeLoginPassword/ChangeLoginPasswordPresenter.kt
+++ app/src/main/java/com/zillennium/utswap/module/security/securityFragment/changeLoginPassword/ChangeLoginPasswordPresenter.kt
@@ -2,13 +2,39 @@ package com.zillennium.utswap.module.security.securityFragment.changeLoginPasswo
 
 import android.content.Context
 import android.os.Bundle
+import com.gis.z1android.api.errorhandler.CallbackWrapper
+import com.zillennium.utswap.UTSwapApp
+import com.zillennium.utswap.api.manager.ApiManager
+import com.zillennium.utswap.api.manager.ApiUserImp
 import com.zillennium.utswap.bases.mvp.BaseMvpPresenterImpl
+import com.zillennium.utswap.models.userService.User
+import rx.Subscription
 
 class ChangeLoginPasswordPresenter : BaseMvpPresenterImpl<ChangeLoginPasswordView.View>(),
     ChangeLoginPasswordView.Presenter {
+
+    private var subscription: Subscription? = null
+
     override fun initViewPresenter(context: Context, bundle: Bundle?) {
         mBundle = bundle
         mContext = context
         mView?.initView()
+    }
+
+    override fun onChangeLoginPassword(body: User.ChangeLoginPasswordObject, context: Context) {
+        subscription?.unsubscribe()
+        subscription = ApiUserImp().changeLoginPassword(body,context).subscribe({
+            if(it.status == 1){
+                mView?.onChangePasswordSuccess(it)
+            }else{
+                mView?.onChangePasswordFail(it)
+            }
+        },{
+            object : CallbackWrapper(it, UTSwapApp.instance, arrayListOf()){
+                override fun onCallbackWrapper(status: ApiManager.NetworkErrorStatus, data: Any) {
+                    mView?.onFail(data.toString())
+                }
+            }
+        })
     }
 }
