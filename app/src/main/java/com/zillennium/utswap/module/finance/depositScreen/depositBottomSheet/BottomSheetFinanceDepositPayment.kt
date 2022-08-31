485d34d00148302758803e4d0633ee15bfb13acd
Sokheng chhoeurn <>
2022-08-31 16:37:44 +0700

    Fixed Bugs

--- app/src/main/java/com/zillennium/utswap/module/finance/depositScreen/depositBottomSheet/BottomSheetFinanceDepositPayment.kt
+++ app/src/main/java/com/zillennium/utswap/module/finance/depositScreen/depositBottomSheet/BottomSheetFinanceDepositPayment.kt
@@ -355,7 +355,7 @@ class BottomSheetFinanceDepositPayment : BottomSheetDialogFragment(),
                 )
             }
             if (it.status==0){
-                Toast.makeText(context, "Please Check Customer Truename.",Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "${it.message}",Toast.LENGTH_SHORT).show()
                 binding?.progressBar?.visibility =View.GONE
             }
 
@@ -369,23 +369,4 @@ class BottomSheetFinanceDepositPayment : BottomSheetDialogFragment(),
     }
 }
 
-//class DecimalDigitsInputFilter(digitsBeforeZero: Int, digitsAfterZero: Int) :
-//    InputFilter {
-//    var mPattern: Pattern
-//    override fun filter(
-//        source: CharSequence,
-//        start: Int,
-//        end: Int,
-//        dest: Spanned,
-//        dstart: Int,
-//        dend: Int
-//    ): CharSequence? {
-//        val matcher: Matcher = mPattern.matcher(dest)
-//        return if (!matcher.matches()) "" else null
-//    }
-//
-//    init {
-//        mPattern =
-//            Pattern.compile("[0-9]{0," + (digitsBeforeZero - 1) + "}+((\\.[0-9]{0," + (digitsAfterZero - 1) + "})?)||(\\.)?")
-//    }
-//}
+
