8ab056c3a742b6f77bbe0b2f22f86170c4c30b33
Eangly <teeangly77@gmail.com>
2022-09-19 14:00:32 +0700

    click upcoming project

--- app/src/main/java/com/zillennium/utswap/module/main/trade/tradeExchangeScreen/fragment/chart/ChartFragment.kt
+++ app/src/main/java/com/zillennium/utswap/module/main/trade/tradeExchangeScreen/fragment/chart/ChartFragment.kt
@@ -1,6 +1,8 @@
 package com.zillennium.utswap.module.main.trade.tradeExchangeScreen.fragment.chart
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
@@ -59,6 +61,11 @@ class ChartFragment :
 
             webView.webChromeClient = WebChromeClient()
             webView.webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    binding.progressBar.visibility = View.VISIBLE
+                }
+
                 override fun onPageFinished(view: WebView, url: String) {
                     injectCssIntoWebView(
                         webView,
@@ -68,6 +75,7 @@ class ChartFragment :
                         )
                     )
                     super.onPageFinished(view, url)
+                    binding.progressBar.visibility = View.GONE
                 }
             }
 //              webView.addJavascriptInterface(AndroidJSInterface, "Android");
--- app/src/main/java/com/zillennium/utswap/module/main/trade/tradeScreen/adapter/TradeUpcomingProjectAdapter.kt
+++ app/src/main/java/com/zillennium/utswap/module/main/trade/tradeScreen/adapter/TradeUpcomingProjectAdapter.kt
@@ -7,14 +7,30 @@ import android.view.ViewGroup
 import com.zillennium.utswap.bases.mvp.BaseRecyclerViewAdapterGeneric
 import com.zillennium.utswap.bases.mvp.BaseViewHolder
 import com.zillennium.utswap.databinding.ItemListTradeUpcomingProjectBinding
+import com.zillennium.utswap.models.TradeModel
 import com.zillennium.utswap.models.tradingList.TradingList
+import com.zillennium.utswap.module.project.projectInfoScreen.ProjectInfoActivity
 
-class TradeUpcomingProjectAdapter : BaseRecyclerViewAdapterGeneric<TradingList.TradeUpComingProjectList, TradeUpcomingProjectAdapter.ItemViewHolder>(){
+class TradeUpcomingProjectAdapter() : BaseRecyclerViewAdapterGeneric<TradingList.TradeUpComingProjectList, TradeUpcomingProjectAdapter.ItemViewHolder>(){
     inner class ItemViewHolder(root: ItemListTradeUpcomingProjectBinding): BaseViewHolder<ItemListTradeUpcomingProjectBinding>(root)
     {
         fun bindData(tradeList: TradingList.TradeUpComingProjectList,position: Int){
             binding.apply {
                 txtProject.text = tradeList.project_name
+
+                txtDetail.setOnClickListener {
+                    ProjectInfoActivity.launchProjectInfoActivity(
+                        root.context,
+                        tradeList.id,
+                    )
+                }
+
+                txtSubscribe.setOnClickListener {
+                    ProjectInfoActivity.launchProjectInfoActivity(
+                        root.context,
+                        tradeList.id,
+                    )
+                }
 
                 if(position + 1 == items.size){
                     viewLine.visibility = View.GONE
@@ -31,5 +47,9 @@ class TradeUpcomingProjectAdapter : BaseRecyclerViewAdapterGeneric<TradingList.T
 
     override fun onBindItemHolder(holder: ItemViewHolder, position: Int, context: Context) {
         holder.bindData(items[position],position)
+    }
+
+    interface Listener{
+        fun clickMe(tradeList: TradingList.TradeUpComingProjectList)
     }
 }
